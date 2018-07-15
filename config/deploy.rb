15cd7f167b62ce7da65f1d87424e64cf89fba310
bergren2 <dan@heymrbass.com>
2018-07-15 13:18:40 -0500

    set pty true

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :migration_role, :app
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
